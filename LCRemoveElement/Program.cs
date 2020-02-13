c5cce09aa914c4c0ee57a76e2e48b5963355917f
jeremy_plott <plott.j@pg.com>
2020-02-13 14:04:30 -0500

    Add project files.

---
+++ LCRemoveElement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LCRemoveElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 3, 2, 2, 3, 4, 5, 3, 6 };
+
+            int val = 3;
+
+            RemoveElement(nums, val);
+
+            for (int num = 0; num < nums.Length; num++)
+            {
+                Console.WriteLine(nums[num]);
+            }
+
+            int RemoveElement(int[] nums, int val)
+            {
+                var arrayWriter = 0;
+
+                for (int index = 0; index < nums.Length; index++)
+                {
+                    if (nums[index] == val)
+                    {
+                        continue;
+                    }
+
+                    nums[arrayWriter] = nums[index];
+                    arrayWriter++;
+                }
+
+                return arrayWriter;
+            }
+        }
+    }
+}
